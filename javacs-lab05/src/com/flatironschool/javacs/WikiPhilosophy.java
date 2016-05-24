61978b570bd4391d5624c8ca02113bc082a046f8
SheldonLau <sheldonlau1@gmail.com>
2016-05-24 00:49:57 -0700

    added comments

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -20,9 +20,9 @@ public class WikiPhilosophy {
 	 * https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
 	 * 
 	 * 1. Clicking on the first non-parenthesized, non-italicized link
-     * 2. Ignoring external links, links to the current page, or red links
-     * 3. Stopping when reaching "Philosophy", a page with no links or a page
-     *    that does not exist, or when a loop occurs
+   * 2. Ignoring external links, links to the current page, or red links
+   * 3. Stopping when reaching "Philosophy", a page with no links or a page
+   *    that does not exist, or when a loop occurs
 	 * 
 	 * @param args
 	 * @throws IOException
@@ -30,22 +30,65 @@ public class WikiPhilosophy {
 	public static void main(String[] args) throws IOException {
 		
         // some example code to get you started
-
+    ArrayList urlList = new ArrayList<String>();
 		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+    urlList.add(url);
 		Elements paragraphs = wf.fetchWikipedia(url);
 
 		Element firstPara = paragraphs.get(0);
-		
+    int iterate = 0;
 		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
+    boolean validPath = true;
+    boolean linkFound = false;
+    boolean destinationReached = false;
+    while(!destinationReached) {
+      int leftParens = 0;
+      int rightParens = 0;
+      int count = 0;
+      for (Node node: iter) {
+        linkFound = false;
+        if (node instanceof TextNode) {
+//          System.out.print(node);
+          TextNode text = (TextNode) node;
+          String str = text.text();
+          for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == '(') {
+              leftParens++;
+            }
+            if(str.charAt(i) == ')') {
+              rightParens++;
+            }
+          }
         }
+        if(node instanceof Element && count > 0) {
+          Element element = (Element)node;
+          Elements links = element.select("a[href]");
+          
+          if(!links.isEmpty() && leftParens == rightParens) {
+            url = links.get(0).attr("abs:href");
+            urlList.add(url);
+            paragraphs = wf.fetchWikipedia(url);
+            firstPara = paragraphs.get(0);
+            iter = new WikiNodeIterable(firstPara);
+            linkFound = true;
+            if(links.get(0).text().equals("philosophy")) {
+              destinationReached = true;
+            }
+            break;
+          }
+        }
+        count++;
+      }
+      if(!linkFound || destinationReached) {
+        validPath = false;
+      }
+    }
+    for(Object val : urlList) {
+      System.out.println(val);
+    }
+  }
 
         // the following throws an exception so the test fails
         // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
-	}
+        //throw new UnsupportedOperationException(msg);
 }
